7975f6bf8df5f889750b6083f0508fe315583255
Recep Batur <recep_btr44@hotmail.com>
2023-07-21 17:37:46 +0300

    AdminLayout alanlarında partialview düzenlemeleri yapıldı.

--- Core_Proje/Controllers/AdminController.cs
+++ Core_Proje/Controllers/AdminController.cs
@@ -12,5 +12,21 @@ namespace Core_Proje.Controllers
         {
             return PartialView();
         }
+        public PartialViewResult PartialFooter()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialNavbar()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialHeader()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialScript()
+        {
+            return PartialView();
+        }
     }
 }

2322cf7cd48a364fdff9f18a38d2835dcb4593aa
Recep Batur <recep_btr44@hotmail.com>
2023-07-23 17:03:32 +0300

    Experience delete and update.

--- Core_Proje/Controllers/ExperienceController.cs
+++ Core_Proje/Controllers/ExperienceController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -18,6 +19,41 @@ namespace Core_Proje.Controllers
             ViewBag.v3 = "Deneyim Listesi";
             var values = experienceManager.TGetList();
             return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddExperience()
+        {
+            ViewBag.v1 = "Deneyim Ekleme";
+            ViewBag.v2 = "Deneyimler";
+            ViewBag.v3 = "Deneyim Ekleme";
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddExperience(Experience experience)
+        {
+            experienceManager.TAdd(experience);
+            return RedirectToAction("Index");
+        }
+        public IActionResult DeleteExperience(int id)
+        {
+            var values = experienceManager.TGetByID(id);
+            experienceManager.TDelete(values);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult UpdateExperience(int id)
+        {
+            ViewBag.v1 = "Deneyim Güncelleme";
+            ViewBag.v2 = "Deneyimler";
+            ViewBag.v3 = "Deneyim Güncelleme";
+            var values = experienceManager.TGetByID(id);
+            return View(values);
+        }
+        [HttpPost]
+        public IActionResult UpdateExperience(Experience experience)
+        {
+            experienceManager.TUpdate(experience);
+            return RedirectToAction("Index");
         }
     }
 }

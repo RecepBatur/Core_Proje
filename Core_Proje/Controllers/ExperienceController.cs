820cf8234fbf8d6e81a37734f2672255fe3e8571
Recep Batur <recep_btr44@hotmail.com>
2023-07-23 16:24:12 +0300

    Experience list.

---
+++ Core_Proje/Controllers/ExperienceController.cs
@@ -0,0 +1,23 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core_Proje.Controllers
+{
+    public class ExperienceController : Controller
+    {
+        ExperienceManager experienceManager = new ExperienceManager(new EfExperienceDal());
+        public IActionResult Index()
+        {
+            ViewBag.v1 = "Deneyim Listesi";
+            ViewBag.v2 = "Deneyimler";
+            ViewBag.v3 = "Deneyim Listesi";
+            var values = experienceManager.TGetList();
+            return View(values);
+        }
+    }
+}

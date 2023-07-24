406f13b16237ce63efb0eb67acb46745ba90d257
Recep Batur <recep_btr44@hotmail.com>
2023-07-24 14:08:46 +0300

    Project added.

--- Core_Proje/Controllers/PortfolioController.cs
+++ Core_Proje/Controllers/PortfolioController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -18,6 +19,20 @@ namespace Core_Proje.Controllers
             ViewBag.v3 = "Proje Listesi";
             var values = portfolioManager.TGetList();
             return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddPortfolio()
+        {
+            ViewBag.v1 = "Proje Ekleme";
+            ViewBag.v2 = "Projeler";
+            ViewBag.v3 = "Proje Ekleme";
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddPortfolio(Portfolio p)
+        {
+            portfolioManager.TAdd(p);
+            return RedirectToAction("Index");
         }
     }
 }

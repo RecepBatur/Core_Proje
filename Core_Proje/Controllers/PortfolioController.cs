2f7300966b6b7987f495163fa0d7225d3702801d
Recep Batur <recep_btr44@hotmail.com>
2023-07-24 13:49:25 +0300

    Portflolio and project list.

---
+++ Core_Proje/Controllers/PortfolioController.cs
@@ -0,0 +1,23 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core_Proje.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        PortfolioManager portfolioManager = new PortfolioManager(new EfPortfolioDal());
+        public IActionResult Index()
+        {
+            ViewBag.v1 = "Proje Listesi";
+            ViewBag.v2 = "Projeler";
+            ViewBag.v3 = "Proje Listesi";
+            var values = portfolioManager.TGetList();
+            return View(values);
+        }
+    }
+}

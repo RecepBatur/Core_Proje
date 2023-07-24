4f3dc8021273d9bb3deaf9105c0235e7af1689f7
Recep Batur <recep_btr44@hotmail.com>
2023-07-24 17:58:52 +0300

    Portfolio update.

--- Core_Proje/Controllers/PortfolioController.cs
+++ Core_Proje/Controllers/PortfolioController.cs
@@ -25,14 +25,14 @@ namespace Core_Proje.Controllers
         [HttpGet]
         public IActionResult AddPortfolio()
         {
+            ViewBag.v1 = "Proje Ekleme";
+            ViewBag.v2 = "Projeler";
+            ViewBag.v3 = "Proje Ekleme";
             return View();
         }
         [HttpPost]
         public IActionResult AddPortfolio(Portfolio p)
         {
-            ViewBag.v1 = "Proje Ekleme";
-            ViewBag.v2 = "Projeler";
-            ViewBag.v3 = "Proje Ekleme";
 
             PortfolioValidator validations = new PortfolioValidator();
             ValidationResult results = validations.Validate(p);
@@ -56,6 +56,38 @@ namespace Core_Proje.Controllers
             var value = portfolioManager.TGetByID(id);
             portfolioManager.TDelete(value);
             return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult UpdatePortfolio(int id)
+        {
+            ViewBag.v1 = "Proje Güncelleme";
+            ViewBag.v2 = "Projeler";
+            ViewBag.v3 = "Proje Güncelleme";
+
+            var values = portfolioManager.TGetByID(id);
+            return View(values);
+        }
+        [HttpPost]
+        public IActionResult UpdatePortfolio(Portfolio portfolio)
+        {
+            PortfolioValidator validations = new PortfolioValidator();
+            ValidationResult results = validations.Validate(portfolio);
+            if (results.IsValid)
+            {
+                portfolioManager.TUpdate(portfolio);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+
+            return View();
+
+
         }
     }
 }

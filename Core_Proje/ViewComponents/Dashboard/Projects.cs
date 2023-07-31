39627df5a9620f2219faa24b7ad727fef2157b8d
Recep Batur <recep_btr44@hotmail.com>
2023-07-31 17:09:38 +0300

    last five project listed.

---
+++ Core_Proje/ViewComponents/Dashboard/Projects.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core_Proje.ViewComponents.Dashboard
+{
+    public class Projects : ViewComponent
+    {
+        PortfolioManager portfolioManager = new PortfolioManager(new EfPortfolioDal());
+
+        public IViewComponentResult Invoke()
+        {
+            var values = portfolioManager.TGetList().OrderByDescending(x => x.PortfolioID).Take(5).ToList();
+            return View(values);
+        }
+    }
+}

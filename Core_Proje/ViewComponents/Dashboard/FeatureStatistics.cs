0066ce65c96622418c520f22da7d08554370645f
Recep Batur <recep_btr44@hotmail.com>
2023-07-31 16:42:59 +0300

    statistics list.

--- Core_Proje/ViewComponents/Dashboard/FeatureStatistics.cs
+++ Core_Proje/ViewComponents/Dashboard/FeatureStatistics.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,8 +9,14 @@ namespace Core_Proje.ViewComponents.Dashboard
 {
     public class FeatureStatistics : ViewComponent
     {
+        Context c = new Context();
+
         public IViewComponentResult Invoke()
         {
+            ViewBag.v1 = c.Skills.Count();
+            ViewBag.v2 = c.Messages.Where(x => x.Status == false).Count();
+            ViewBag.v3 = c.Messages.Where(x => x.Status == true).Count();
+            ViewBag.v4 = c.Experiences.Count();
             return View();
         }
     }

b2260b1fdcd09bc013d2c8f680916b80bb1bc62f
Recep Batur <recep_btr44@hotmail.com>
2023-08-18 01:32:18 +0300

    profile page image ınstall

--- Core_Proje/Areas/Writer/Controllers/ProfileController.cs
+++ Core_Proje/Areas/Writer/Controllers/ProfileController.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Core_Proje.Areas.Writer.Models;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,8 +13,45 @@ namespace Core_Proje.Areas.Writer.Controllers
     [Area("Writer")]
     public class ProfileController : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<WriterUser> _userManager;
+
+        public ProfileController(UserManager<WriterUser> userManager)
         {
+            _userManager = userManager;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name); //Sisteme giriş yapan kullanıcıyı buluyoruz.
+            UserEditViewModel model = new UserEditViewModel();
+            model.Name = values.Name;
+            model.Surname = values.Surname;
+            model.PictureUrl = values.ImageUrl;
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(UserEditViewModel p)
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            if (p.Picture != null)
+            {
+                var resource = Directory.GetCurrentDirectory(); //resim yolunu al
+                var extension = Path.GetExtension(p.Picture.FileName); //parametreden gelen picture'ın filename'ini al dedik.
+                var imageName = Guid.NewGuid() + extension; // benzersiz resim ismini oluşturduk.
+                var saveLocation = resource + "/wwwroot/UserImage/" + imageName; // resmi nereye kaydediyor onu belirledik.
+                var stream = new FileStream(saveLocation, FileMode.Create); //resim dosyasını ekledik.
+                await p.Picture.CopyToAsync(stream); //resmi kopyaladık.
+                user.ImageUrl = imageName;
+            }
+
+            user.Name = p.Name;
+            user.Surname = p.Surname;
+
+            var result = await _userManager.UpdateAsync(user);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Default");
+            }
             return View();
         }
     }

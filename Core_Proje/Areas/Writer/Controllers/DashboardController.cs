3817ef84bcde5c77766d9d5122af3b7f7e4e4673
Recep Batur <recep_btr44@hotmail.com>
2023-08-18 17:56:54 +0300

    User name and surname viewbag.

---
+++ Core_Proje/Areas/Writer/Controllers/DashboardController.cs
@@ -0,0 +1,28 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core_Proje.Areas.Writer.Controllers
+{
+    [Area("Writer")]
+    public class DashboardController : Controller
+    {
+        private readonly UserManager<WriterUser> _userManager;
+
+        public DashboardController(UserManager<WriterUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name); //Sisteme giriş yapan kullanıcıyı buluyoruz.
+            ViewBag.v = values.Name + " " + values.Surname;
+            return View();
+        }
+    }
+}

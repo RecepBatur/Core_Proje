f93b282cdf82657066a0878f366566000ada787b
Recep Batur <recep_btr44@hotmail.com>
2023-08-19 14:14:51 +0300

    Weather forecast api added.

--- Core_Proje/Areas/Writer/Controllers/DashboardController.cs
+++ Core_Proje/Areas/Writer/Controllers/DashboardController.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Core_Proje.Areas.Writer.Controllers
 {
@@ -24,6 +25,11 @@ namespace Core_Proje.Areas.Writer.Controllers
             var values = await _userManager.FindByNameAsync(User.Identity.Name); //Sisteme giriş yapan kullanıcıyı buluyoruz.
             ViewBag.v = values.Name + " " + values.Surname;
 
+            //Weather API
+            string api = "29d18df7b4069aef0b70abeed6761c24";
+            string connection = " https://api.openweathermap.org/data/2.5/weather?q=istanbul&mode=xml&lang=tr&units=metric&appid=" + api;
+            XDocument document = XDocument.Load(connection);
+            ViewBag.v5 = document.Descendants("temperature").ElementAt(0).Attribute("value").Value;
 
             //statistics
             Context c = new Context();
@@ -35,3 +41,7 @@ namespace Core_Proje.Areas.Writer.Controllers
         }
     }
 }
+
+/*
+ https://api.openweathermap.org/data/2.5/weather?q=istanbul&mode=xml&lang=tr&units=metric&appid=29d18df7b4069aef0b70abeed6761c24
+*/

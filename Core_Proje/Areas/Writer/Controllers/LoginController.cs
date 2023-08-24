9f377f2d3f51214c82b7e8b7f2a88addc18a7ca5
Recep Batur <recep_btr44@hotmail.com>
2023-08-24 14:34:26 +0300

    Area route regulation.

--- Core_Proje/Areas/Writer/Controllers/LoginController.cs
+++ Core_Proje/Areas/Writer/Controllers/LoginController.cs
@@ -10,6 +10,7 @@ using System.Threading.Tasks;
 namespace Core_Proje.Areas.Writer.Controllers
 {
     [Area("Writer")]
+    [Route("Writer/[controller]/[action]")]
     public class LoginController : Controller
     {
         private readonly SignInManager<WriterUser> _signInManager;
@@ -31,7 +32,7 @@ namespace Core_Proje.Areas.Writer.Controllers
                 var result = await _signInManager.PasswordSignInAsync(p.Username, p.Password, true, true);
                 if (result.Succeeded)
                 {
-                    return RedirectToAction("Index", "Default");
+                    return RedirectToAction("Index", "Profile");
                 }
                 else
                 {
--- Core_Proje/Areas/Writer/Controllers/ProfileController.cs
+++ Core_Proje/Areas/Writer/Controllers/ProfileController.cs
@@ -11,6 +11,7 @@ using System.Threading.Tasks;
 namespace Core_Proje.Areas.Writer.Controllers
 {
     [Area("Writer")]
+    [Route("Writer/[controller]/[action]")]
     public class ProfileController : Controller
     {
         private readonly UserManager<WriterUser> _userManager;
@@ -50,7 +51,7 @@ namespace Core_Proje.Areas.Writer.Controllers
             var result = await _userManager.UpdateAsync(user);
             if (result.Succeeded)
             {
-                return RedirectToAction("Index", "Default");
+                return RedirectToAction("Index", "Login");
             }
             return View();
         }

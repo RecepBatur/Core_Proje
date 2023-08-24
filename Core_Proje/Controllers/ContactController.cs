ef515c27f475f5170b8cc411ded3cdf39a919997
Recep Batur <recep_btr44@hotmail.com>
2023-08-25 01:20:49 +0300

    Contact form crud regulations.

---
+++ Core_Proje/Controllers/ContactController.cs
@@ -0,0 +1,39 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core_Proje.Controllers
+{
+    public class ContactController : Controller
+    {
+        MessageManager messageManager = new MessageManager(new EfMessageDal());
+        public IActionResult Index()
+        {
+            var values = messageManager.TGetList();
+            return View(values);
+        }
+        public IActionResult DeleteContact(int id)
+        {
+            var values = messageManager.TGetByID(id);
+            messageManager.TDelete(values);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult UpdateContact(int id)
+        {
+            var values = messageManager.TGetByID(id);
+            return View(values);
+        }
+        [HttpPost]
+        public IActionResult UpdateContact(Message message)
+        {
+            messageManager.TUpdate(message);
+            return RedirectToAction("Index");
+        }
+    }
+}

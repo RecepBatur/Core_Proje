92ced7d5fae4a1d59015d8275948386dd82d8f04
Recep Batur <recep_btr44@hotmail.com>
2023-08-25 15:59:51 +0300

    Admin message crud regulation.

---
+++ Core_Proje/Controllers/AdminMessageController.cs
@@ -0,0 +1,40 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core_Proje.Controllers
+{
+    public class AdminMessageController : Controller
+    {
+        WriterMessageManager writerMessageManager = new WriterMessageManager(new EfWriterMessageDal());
+        public IActionResult ReceiverMessageList()
+        {
+            string p;
+            p = "admin@gmail.com";
+            var values = writerMessageManager.GetListReceiverMessage(p);
+            return View(values);
+        }
+        public IActionResult SenderMessageList()
+        {
+            string p;
+            p = "admin@gmail.com";
+            var values = writerMessageManager.GetListReceiverMessage(p);
+            return View(values);
+        }
+        public IActionResult DeleteAdminMessage(int id)
+        {
+            var values = writerMessageManager.TGetByID(id);
+            writerMessageManager.TDelete(values);
+            return RedirectToAction("SenderMessageList");
+        }
+        public IActionResult AdminMessageDetails(int id)
+        {
+            var values = writerMessageManager.TGetByID(id);
+            return View(values);
+        }
+    }
+}

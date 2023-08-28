b755e9e4348aa8d0b870a3a377da3387ce5ba2c6
Recep Batur <recep_btr44@hotmail.com>
2023-08-28 14:59:18 +0300

    Authorize transactions.

--- Core_Proje/Startup.cs
+++ Core_Proje/Startup.cs
@@ -1,8 +1,11 @@
 using DataAccessLayer.Concrete;
 using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -28,6 +31,31 @@ namespace Core_Proje
             services.AddDbContext<Context>();
             services.AddIdentity<WriterUser, WriterRole>().AddEntityFrameworkStores<Context>();
             services.AddControllersWithViews();
+
+            services.AddMvc(config =>
+            {
+                var policy = new AuthorizationPolicyBuilder()
+                .RequireAuthenticatedUser()
+                .Build();
+
+                config.Filters.Add(new AuthorizeFilter(policy));
+            });
+
+            services.AddMvc();
+            services.AddAuthentication(
+                CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(x =>
+                {
+                    x.LoginPath = "/AdminLogin/Index/"; //eðer ki sisteme giriþ yapýlmadýysa buraya yönledirecek.
+                });
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.Cookie.HttpOnly = true;
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(10); //sisteme giriþ yapan kullanýcý ne kadar süre sistemde kalabilsin. 10 dk dedik.
+
+                options.LoginPath = "/Writer/Login/Index/"; //kiþi önce sisteme giriþ yapacak.
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

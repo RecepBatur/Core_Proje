012cd1964c34ee6e56412e70e1b288b7705d2062
Recep Batur <recep_btr44@hotmail.com>
2023-08-29 13:43:12 +0300

    Experience and WriterUser ajax transactions.

--- Core_Proje/Controllers/ExperienceAjaxController.cs
+++ Core_Proje/Controllers/ExperienceAjaxController.cs
@@ -10,22 +10,22 @@ using System.Threading.Tasks;
 
 namespace Core_Proje.Controllers
 {
-    public class WriterUserController : Controller
+    public class ExperienceAjaxController : Controller
     {
-        WriterUserManager writerUserManager = new WriterUserManager(new EfWriterUserDal());
+        ExperienceManager experienceManager = new ExperienceManager(new EfExperienceDal());
         public IActionResult Index()
         {
             return View();
         }
-        public IActionResult ListWriterUser()
+        public IActionResult ListExperience()
         {
-            var values = JsonConvert.SerializeObject(writerUserManager.TGetList());
+            var values = JsonConvert.SerializeObject(experienceManager.TGetList());
             return Json(values);
         }
         [HttpPost]
-        public IActionResult AddUser(WriterUser p)
+        public IActionResult AddExperience(Experience p)
         {
-            writerUserManager.TAdd(p);
+            experienceManager.TAdd(p);
             var values = JsonConvert.SerializeObject(p);
             return Json(values);
         }

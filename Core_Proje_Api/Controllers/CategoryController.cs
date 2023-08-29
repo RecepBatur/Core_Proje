1db43b2fe3c66e41804f1cf3e16198f94ce3e27c
Recep Batur <recep_btr44@hotmail.com>
2023-08-30 00:32:33 +0300

    Api CategoryAdd added.

--- Core_Proje_Api/Controllers/CategoryController.cs
+++ Core_Proje_Api/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using Core_Proje_Api.DAL.ApiContext;
+using Core_Proje_Api.DAL.Entity;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -19,7 +20,7 @@ namespace Core_Proje_Api.Controllers
             return Ok(c.Categories.ToList());
         }
         [HttpGet("{id}")]
-        public IActionResult CategoryGetById(int id)
+        public IActionResult CategoryGet(int id)
         {
             using var c = new Context();
             var value = c.Categories.Find(id);
@@ -32,6 +33,14 @@ namespace Core_Proje_Api.Controllers
                 return Ok(value);
             }
 
+        }
+        [HttpPost]
+        public IActionResult CategoryAdd(Category p)
+        {
+            using var c = new Context();
+            c.Add(p);
+            c.SaveChanges();
+            return Created("", p);
         }
     }
 }
